c84b7796825fbabbfc5d8ad0576b5e585388ef4d
Stefan Schlager <zarquon42@gmail.com>
2022-01-19 15:19:29 +0100

    Script: added build option and included check if submodule is initialized (and initialize it if not)

--- Script_StandardsPatho-Robot.sh
+++ Script_StandardsPatho-Robot.sh
@@ -1,22 +1,21 @@
 #! /bin/bash
 cleanup=0
 update=0
+build=0
 function usage {
     echo " "
-    echo "usage: $0 [-c ][ -u]"
+    echo "usage: $0 [-b][-c][-u]"
     echo " "
-    
+    echo "    -b          build owl file"
     echo "    -c          cleanup temp files"
     echo "    -u          initalize/update submodule"
     echo "    -h -?       print this help"
-    
-
     echo " "
     
     exit
 }
 
-while getopts "cuh?" opt; do
+while getopts "bcuh?" opt; do
     case "$opt" in
         c)
             cleanup=1
@@ -24,75 +23,84 @@ while getopts "cuh?" opt; do
             ;;
 	u)  update=1
 	    ;;
-       
+	b) build=1
+	   ;;       
 	?)
-	    usage
-	    ;;
+	usage
+	;;
 	h)
 	    usage
 	    ;;
-	
-	
-	    
-	    
     esac
 done
+if [ -z "$1" ]; then
+    usage
+fi
+## check if submodule is initialized
 
+gitchk=$(git submodule foreach 'echo $sm_path `git rev-parse HEAD`')
+if [ -z "$gitchk" ];then
+    update=1
+    echo "Initializing git submodule"
+fi
+
+## Initialize and update git submodule
 if  [ $update -eq 1 ]; then
     git submodule init
     git submodule update
 fi
 
+### Build ontologies
+if [ $build -eq 1 ]; then
+    ## Merge Core Ontology
 
+    robot merge --input ./dependencies/RDFBones/RDFBones-main.owl \
+	  --input ./dependencies/RDFBones/FMA-RDFBonesSubset.owl \
+	  --input ./dependencies/RDFBones/RDFBones-ROIs-EntireBoneOrgan.owl \
+	  --input ./dependencies/RDFBones/OBI-RDFBonesSubset.owl \
+	  --input ./dependencies/RDFBones/CIDOC-CRM-RDFBonesSubset.owl \
+	  --input ./dependencies/RDFBones/SIO-RDFBonesSubset.owl \
+	  --output results/Merged_CoreOntology.owl
 
-## Merge Core Ontology
+    ## CATEGORY LABELS
 
-robot merge --input ./dependencies/RDFBones/RDFBones-main.owl \
-      --input ./dependencies/RDFBones/FMA-RDFBonesSubset.owl \
-      --input ./dependencies/RDFBones/RDFBones-ROIs-EntireBoneOrgan.owl \
-      --input ./dependencies/RDFBones/OBI-RDFBonesSubset.owl \
-      --input ./dependencies/RDFBones/CIDOC-CRM-RDFBonesSubset.owl \
-      --input ./dependencies/RDFBones/SIO-RDFBonesSubset.owl \
-      --output results/Merged_CoreOntology.owl
+    robot template --template Template_StandardsPatho-CategoryLabels.tsv \
+	  --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
+	  --prefix "fma: http://purl.org/sig/ont/fma/" \
+	  --prefix "obo: http://purl.obolibrary.org/obo/" \
+	  --prefix "standards-si: http://w3id.org/rdfbones/ext/standards-si/" \
+	  --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
+	  --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
+	  --output results/StandardsPatho_CategoryLabels.owl
 
-## CATEGORY LABELS
+    ## VALUE SPECIFICATIONS
 
-robot template --template Template_StandardsPatho-CategoryLabels.tsv \
-      --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
-      --prefix "fma: http://purl.org/sig/ont/fma/" \
-      --prefix "obo: http://purl.obolibrary.org/obo/" \
-      --prefix "standards-si: http://w3id.org/rdfbones/ext/standards-si/" \
-      --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
-      --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
-      --output results/StandardsPatho_CategoryLabels.owl
+    ## Merge category labels into core ontology
 
-## VALUE SPECIFICATIONS
+    robot merge --input results/Merged_CoreOntology.owl \
+	  --input results/StandardsPatho_CategoryLabels.owl \
+	  --output results/Merged_CategoryLabels.owl
 
-## Merge category labels into core ontology
+    # Create value specifications
 
-robot merge --input results/Merged_CoreOntology.owl \
-      --input results/StandardsPatho_CategoryLabels.owl \
-      --output results/Merged_CategoryLabels.owl
+    robot template --template Template_StandardsPatho-ValueSpecifications.tsv \
+	  --input results/Merged_CategoryLabels.owl \
+	  --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
+	  --prefix "obo: http://purl.obolibrary.org/obo/" \
+	  --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
+	  --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
+	  --output results/StandardsPatho_ValueSpecifications.owl
 
-# Create value specifications
+    ## EXTENSION
 
-robot template --template Template_StandardsPatho-ValueSpecifications.tsv \
-      --input results/Merged_CategoryLabels.owl \
-      --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
-      --prefix "obo: http://purl.obolibrary.org/obo/" \
-      --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
-      --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
-      --output results/StandardsPatho_ValueSpecifications.owl
+    robot merge --input results/StandardsPatho_CategoryLabels.owl \
+	  --input results/StandardsPatho_ValueSpecifications.owl \
+	  --output results/standards-patho.owl
 
-## EXTENSION
-
-robot merge --input results/StandardsPatho_CategoryLabels.owl \
-      --input results/StandardsPatho_ValueSpecifications.owl \
-      --output results/standards-patho.owl
+   
+    
+fi
 
 if  [ $cleanup -eq 1 ]; then
-    rm results/Merged_CoreOntology.owl results/Merged_CategoryLabels.owl results/StandardsPatho_CategoryLabels.owl results/StandardsPatho_ValueSpecifications.owl
+	rm results/Merged_CoreOntology.owl results/Merged_CategoryLabels.owl results/StandardsPatho_CategoryLabels.owl results/StandardsPatho_ValueSpecifications.owl
 fi
- 
-
-read -p 'Hit ENTER to exit'
