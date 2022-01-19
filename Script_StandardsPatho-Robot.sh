d1fbbd4f108f4004530ef4dc3aaa4844dee748d1
Felix Engel <Felix.Engel@uniklinik-freiburg.de>
2022-01-19 10:32:01 +0100

    Adapted script to work in cloned repositories.

--- Script_StandardsPatho-Robot.sh
+++ Script_StandardsPatho-Robot.sh
@@ -2,24 +2,24 @@
 
 ## Merge Core Ontology
 
-robot merge --input ~/gitprojects/RDFBones-O/RDFBones-main.owl \
-      --input ~/gitprojects/RDFBones-O/FMA-RDFBonesSubset.owl \
-      --input ~/gitprojects/RDFBones-O/RDFBones-ROIs-EntireBoneOrgan.owl \
-      --input ~/gitprojects/RDFBones-O/OBI-RDFBonesSubset.owl \
-      --input ~/gitprojects/RDFBones-O/CIDOC-CRM-RDFBonesSubset.owl \
-      --input ~/gitprojects/RDFBones-O/SIO-RDFBonesSubset.owl \
+robot merge --input ./dependencies/RDFBones/RDFBones-main.owl \
+      --input ./dependencies/RDFBones/FMA-RDFBonesSubset.owl \
+      --input ./dependencies/RDFBones/RDFBones-ROIs-EntireBoneOrgan.owl \
+      --input ./dependencies/RDFBones/OBI-RDFBonesSubset.owl \
+      --input ./dependencies/RDFBones/CIDOC-CRM-RDFBonesSubset.owl \
+      --input ./dependencies/RDFBones/SIO-RDFBonesSubset.owl \
       --output results/Merged_CoreOntology.owl
 
 ## CATEGORY LABELS
 
 robot template --template Template_StandardsPatho-CategoryLabels.tsv \
-  --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
-  --prefix "fma: http://purl.org/sig/ont/fma/" \
-  --prefix "obo: http://purl.obolibrary.org/obo/" \
-  --prefix "standards-si: http://w3id.org/rdfbones/ext/standards-si/" \
-  --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
-  --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
-  --output results/StandardsPatho_CategoryLabels.owl
+      --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
+      --prefix "fma: http://purl.org/sig/ont/fma/" \
+      --prefix "obo: http://purl.obolibrary.org/obo/" \
+      --prefix "standards-si: http://w3id.org/rdfbones/ext/standards-si/" \
+      --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
+      --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
+      --output results/StandardsPatho_CategoryLabels.owl
 
 ## VALUE SPECIFICATIONS
 
@@ -32,17 +32,18 @@ robot merge --input results/Merged_CoreOntology.owl \
 # Create value specifications
 
 robot template --template Template_StandardsPatho-ValueSpecifications.tsv \
-  --input results/Merged_CategoryLabels.owl \
-  --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
-  --prefix "obo: http://purl.obolibrary.org/obo/" \
-  --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
-  --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
-  --output results/StandardsPatho_ValueSpecifications.owl
+      --input results/Merged_CategoryLabels.owl \
+      --prefix "rdfbones: http://w3id.org/rdfbones/core#" \
+      --prefix "obo: http://purl.obolibrary.org/obo/" \
+      --prefix "standards-patho: http://w3id.org/rdfbones/ext/standards-patho/" \
+      --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
+      --output results/StandardsPatho_ValueSpecifications.owl
 
 ## EXTENSION
 
 robot merge --input results/StandardsPatho_CategoryLabels.owl \
       --input results/StandardsPatho_ValueSpecifications.owl \
+      --ontology-iri "http://w3id.org/rdfbones/ext/standards-patho/standards-patho.owl" \
       --output results/standards-patho.owl
 
 read -p 'Hit ENTER to exit'
